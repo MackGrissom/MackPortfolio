24db07a750d87adb045c9b0b05cb8acb30ecff66
Mack Grissom <mcintyregrissom@gmail.com>
2022-10-29 18:10:01 +0700

    updated skill section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,7 +6,7 @@ import { skills } from '../data';
 const Skills = () => {
   return (
     <section className='bg-tertiary py-12'>
-      <h2 className='text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before:opacity-40 before:-top-[2rem] before:hidden before:lg:block center'> My Skills</h2>
+      <h2 className='text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 relative before:absolute before:opacity-40 before:-top-[2rem] before:hidden before:lg:block center'> My Top Technical Skills</h2>
       <div className='container mx-auto'>
         <div
           className='grid grid-cols-7 md:grid-flow-col'
