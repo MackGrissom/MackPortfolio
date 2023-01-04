de90a2eb67021ef1a1f4cf5718fb9d0f01822214
Mack Grissom <mcintyregrissom@gmail.com>
2023-01-04 18:25:47 -0500

    added stylings adjusted buttons

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
       <div className='container mx-auto'>
         <div className='flex flex-col xl:flex-row gap-24'>
           <img
-            className='object-cover h-full w-[566px] md:mx-auto lg:mx-0 rounded-2xl'
+            className='object-cover h-full w-[500px]  md:mx-auto lg:mx-0 rounded-2xl'
             src={Image}
             alt=''
           />
@@ -26,19 +26,18 @@ const About = () => {
               <p className='mb-8'>
                 I'm an ambitious full-stack, object oriented, software engineer. I'm eager to build modern <strong> solutions</strong> for your business. As a recent graduate from one of the most decorated full-stack programming bootcamps, Le Wagon, I am fully equipped to build modern & fully responsive products.
               </p>
-              {/* <p className='mb-8'> I use several technologies, depending on what the project calls for. My main tech stack is the following: React.JS, Tailwind CSS, Ruby, Ruby on Rails, Node.JS. However I know other languages and am eager to co
-              </p> */}
-  
-
-            <p className='mb-8'> In my freetime I enjoy traveling, hiking, surfing, learning new languages, playing guitar, reading, photography and of course, computer programming! </p>
+             
             <p className='mb-8'> I'm open to freelance projects & remote fulltime positions. Please see my resume below & continue down my portfolio to see my tech stack & recent projects. </p>
           </div>
 
           {/* <button className='btn btn-md bg-accent hover:bg-secondary-hover transition-all'>
               Contact me  
            </button> */}
-          <a href='https://app.enhancv.com/share/7cdac89a/?utm_medium=growth&utm_campaign=share-resume&utm_source=dynamic' className='resume'> My Resume</a>
-
+           
+          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
+                <a href='https://app.enhancv.com/share/7cdac89a/?utm_medium=growth&utm_campaign=share-resume&utm_source=dynamic' class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  My Resume
+                </a> </button>
         </div>
       </div>
     </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import woman image
 import ManImg from '../assets/img/banner-man2.webp';
-
+import BG from '../assets/tech.mp4'
 const Hero = () => {
   return (
     <section
       id='home'
-      className='lg:h-[85vh] flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden'
+      className='lg:h-[85vh] flex items-center bg-gradient-to-r from-gray-700 via-gray-900 to-black lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden relative object-cover'
     >
+
       <div className='container mx-auto h-full'>
         <div className='flex items-center h-full pt-8'>
           <div className='flex-1 flex flex-col items-center lg:items-start'>
@@ -16,31 +17,27 @@ const Hero = () => {
               Hey, I'm Mack! 👋
             </p>
             <h1 className='text-4xl leading-[44px] md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px]'>
-              A Full-Stack Software Engineer & Web Developer.
+              I'm A Full-Stack Web Developer.
             </h1>
             <p className='pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] text-lg text-center lg:text-left'>
-              
+              I build modern, fully-responsive Websites and Web apps.
             </p>
-           
-            {/* <button className='btn btn-md bg-accent hover:bg-secondary-hover md:btn-lg transition-all'>
-            <Link
-                to="Contact"
-                activeClass="active"
-                spy={true}
-                smooth={true}
-                duration={800}
-                offset={-70}
-                className="transition-all duration-500"
-              >
-                Work with me
-            </Link>
+            <div className='flex'>
+              <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
+                <a href='contact' class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  Contact Me
+                </a> </button>
+
+                <button type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"><a href='projects'>See My Projects </a></button>
               
-            </button> */}
+            </div>
+
           </div>
           <div className='hidden lg:flex flex-1 justify-end items-end h-full w-2 md:mb-[30%] sm:!mt-0 
           !ml-0 !mr-0 !mb-[200px]'>
-            <img src={ManImg} alt='' />
+            <img src={ManImg} alt='' className='avatar' />
           </div>
+
         </div>
       </div>
     </section>
